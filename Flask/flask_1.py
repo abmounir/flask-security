3231f721bb585cf5bb59458bdc1d61008fbe57b6
Mehdi <mehdidouy@gmail.com>
2019-11-17 18:15:17 +0100

    17/11/1019
    1ST PUSH

---
+++ Flask/flask_1.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/home", methods=['POST'])
+def home():
+    name = request.form["name"]
+    mail = request.form["email"]
+    password = request.form["passw"]
+    if(name != "mehdi"):
+        return "<H1> the name is incorrect a khoya </H1>"
+    elif(password != "mmeehhddii"):
+        return "<H1> Le mot de passe est incorrect XD </H1>"
+
+    return render_template("index2.html", val1="mehdi", val2="this is", val3="my name")
+
+
+@app.route("/")
+def register():
+    return render_template("register.html", val1="mehdi", val2="this is", val3="my name")
+
+
+@app.route("/signin/")
+def signin():
+    return render_template("signin.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
