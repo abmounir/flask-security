19a0363dc2cdef5927365924b7e961fe2ba22e0a
Mehdi <mehdidouy@gmail.com>
2019-12-05 23:36:48 +0100

    finished a good part of it
    b9alna rir verif ta3 l acc ida kayen

--- Flask/securité/captchacreater.py
+++ Flask/securité/captchacreater.py
@@ -1,8 +1,7 @@
 from captcha.image import ImageCaptcha
 from captcha.audio import AudioCaptcha
-#from matplotlib import pyplot as plt
 import random
-import TokenGenerator
+from securité import TokenGenerator
 
 number_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -48,9 +47,9 @@ def create_random_digital_text(captcha_string_size=10):
 
     captcha_string = ''
 
-    #Convert the digital list to string.
+    # Convert the digital list to string.
     for item in captcha_string_list:
-       captcha_string += str(item)
+        captcha_string += str(item)
 
     return captcha_string
 
@@ -66,41 +65,13 @@ def create_image_captcha(captcha_text):
     image_captcha.create_noise_curve(image, image.getcolors())
 
     # Add noise dots for the image.
-    image_captcha.create_noise_dots(image, image.getcolors())
+    #image_captcha.create_noise_dots(image, image.getcolors())
 
     # Save the image to a png file.
     image_file = "./captcha_"+captcha_text + ".png"
     image_captcha.write(captcha_text, image_file)
-
-    print(image_file + " has been created.")
-
-# Create an audio captcha file.
+    return image_file
 
 
-def create_audio_captcha():
-
-    # Create the audio captcha with the specified voice wav file library folder.
-    # Each captcha char should has it's own directory under the specified folder ( such as ./voices),
-    # for example ./voices/a/a.wav will be played when the character is a.
-    # If you do not specify your own voice file library folder, the default built-in voice library which has only digital voice file will be used.
-    # audio_captcha = AudioCaptcha(voicedir='./voices')
-
-    # Create an audio captcha which use digital voice file only.
-    audio_captcha = AudioCaptcha()
-
-    # Because we use the default module voice library, so we can only generate digital text voice.
-    captcha_text = create_random_digital_text()
-
-    # Generate the audio captcha file.
-    audio_data = audio_captcha.generate(captcha_text)
-
-    # Save the autiod captcha file.
-    audio_file = "./captcha_"+captcha_text+'.wav'
-    audio_captcha.write(captcha_text, audio_file)
-
-    print(audio_file + " has been created.")
-
-
-if __name__ == '__main__':
-    x=str(TokenGenerator.getTokenUser('test1@gmail.com'))
-    create_image_captcha(x)
+x = str(TokenGenerator.getTokenUser('test1@gmail.com'))
+create_image_captcha(x)
