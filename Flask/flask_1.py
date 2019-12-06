286e2839a84c2678774edfe1cf7764a1fff58a1e
Mehdi <mehdidouy@gmail.com>
2019-12-06 11:32:04 +0100

    06/12019
    working 90% find some bugs bro

--- Flask/flask_1.py
+++ Flask/flask_1.py
@@ -1,17 +1,20 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 from securité.captchacreater import create_image_captcha
 from securité.sendmail_func import sendMail
 from securité.TokenGenerator import getTokenUser
+from flask import g
+import os
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(21)
 
 
 @app.route("/home", methods=['POST'])
 def home():
     name = request.form["name"]
     mail = request.form["email"]
-
+    session['userMail'] = mail
     password = request.form["passw"]
     if(name != "mehdi"):
         return "<H1> the name is incorrect a khoya </H1>"
@@ -23,19 +26,19 @@ def home():
 
 @app.route("/")
 def register():
+
     return render_template("register.html")
 
 
 @app.route("/account", methods=['POST'])
 def account():
     tkn = request.form["token"]
-    user_tkn = getTokenUser('mehdidouy@gmail.com')
-    print(tkn)
-    print(user_tkn)
+
+    user_tkn = getTokenUser(session['userMail'])
     if tkn == user_tkn:
         return "<h3> wlcm to your acc sir <h3>"
     else:
-        return redirect("/token")
+        return "wrong token sir"
 
 
 @app.route("/signin/")
@@ -45,16 +48,26 @@ def signin():
 
 @app.route("/token", methods=['POST'])
 def token():
-    try:
-        f = open("./securité/message.html", "r")
-        ms = f.read()
-        f.close()
-        mail = request.form["email"]
-        sendMail(mail, "Bank Token", ms)
-        return render_template("token.html")
-    except:
-        return render_template("token.html")
+
+    f = open("./securité/message.html", "r")
+    ms = f.read()
+    f.close()
+    mail = request.form["email"]
+    session['userMail'] = mail
+    print("user session is "+session['userMail'])
+    sendMail(mail, "Bank Token", ms)
+    return render_template("token.html")
+
+
+@app.route("/resend")
+def resendToken():
+    f = open("./securité/message.html", "r")
+    ms = f.read()
+    f.close()
+    sendMail(session['userMail'], "Bank Token", ms)
+    return render_template("token.html")
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
