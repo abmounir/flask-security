d968c30c2ed64049cf0803caf067fe08f57873f3
abmounir <abs.mounir1@gmail.com>
2019-12-20 23:33:47 +0100

    added expired session for 1 minute

--- Flask/models.py
+++ Flask/models.py
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
 	__tablename__ = "password2"
-	mail = db.Column('email', db.String, primary_key=True,default='aaaa@aa.aa')
+	mail = db.Column('email', db.String, primary_key=True,default='tst@aa.aa')
 	password = db.Column('login', db.String)
 
 
@@ -24,7 +24,7 @@ class ConnectClient(db.Model):
 	__bind_key__='token_db'
 	__tablename__ = 'token'
 
-	Email = db.Column('email', db.String, primary_key=True,default='aaaa@aa.aa')
+	Email = db.Column('email', db.String, primary_key=True,default='tst@aa.aa')
 	Token = db.Column('token', db.Integer)
 
 
@@ -33,14 +33,10 @@ class Client(db.Model):
 	__bind_key__='clientInfo_db'
 	__tablename__ = 'utilisateur2'
 
-	Email = db.Column('email', db.String, primary_key=True,default='aaaa@aa.aa')
+	Email = db.Column('email', db.String, primary_key=True,default='tst@aa.aa')
 	Name = db.Column('nom', db.String)
 	Sex=db.Column('sex', db.String)
 	LastName = db.Column('prénom', db.String)
 	CurrentBalance = db.Column('balance', db.String)
 	Incomes = db.Column('incomes', db.String)
 	Expenses = db.Column('expenses', db.String)
-
-
-
-
