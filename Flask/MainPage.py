a73562bc374dfac9d2e0d98131ee69fd7851bb71
Mehdi <mehdidouy@gmail.com>
2019-12-24 11:33:49 +0100

    version 1.5

--- Flask/MainPage.py
+++ Flask/MainPage.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import random
 from array import array
-from models import app,db,Client
+from models import app, db, Client
 import time
 
 # @Author : Mehdi Yc
@@ -12,31 +12,29 @@ getData = Client.query.all()
 
 def getClientData(user):
 
-	x = []
-	for b in getData:
+    x = []
+    for b in getData:
 
-		if str(b.Email) == user:
-			x.append(str(b.Name))
-			x.append(str(b.LastName))
-			x.append(str(b.CurrentBalance))
-			x.append(str(b.Incomes))
-			x.append(str(b.Expenses))
-			print(x)
-		else:
-			print('non')
+        if str(b.Email) == user:
+            x.append(str(b.Name))
+            x.append(str(b.LastName))
+            x.append(str(b.CurrentBalance))
+            x.append(str(b.Incomes))
+            x.append(str(b.Expenses))
+            print(x)
+        else:
+            print('non')
 
-	return x
+    return x
 
-def setClientData(email,nom,prénom,sex,balance,incomes,expenses):
-	new_client=Client(Email=email,Name=nom,LastName=prénom,Sex=sex,CurrentBalance=balance,Incomes=balance,Expenses=expenses)
-	if new_client:
-		db.session.add(new_client)
-		db.session.commit()
-		time.sleep(1)
-		for x in getData :
-			if str(x.Email)=='tst@aa.aa' :
-				deleted=Client.query.filter_by(Email='tst@aa.aa').delete()
-				if deleted:
-					#db.session.delete(deleted)
-					db.session.commit()
-	return new_client
+
+def setClientData(email, nom, prénom, sex, balance, incomes, expenses):
+    new_client = Client(Email=email, Name=nom, LastName=prénom, Sex=sex,
+                        CurrentBalance=balance, Incomes=balance, Expenses=expenses)
+    if new_client:
+        db.session.add(new_client)
+        db.session.commit()
+        time.sleep(1)
+
+    db.session.commit()
+    return new_client
--- Flask/models.py
+++ Flask/models.py
@@ -8,35 +8,39 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Database hosted in remotemysql.com
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://Vpvv8swDIN:Lwj5xUAFl8@remotemysql.com:3306/Vpvv8swDIN"
-app.config['SQLALCHEMY_BINDS']={'token_db':'mysql://RE7esJnNWs:xsODaDhhpG@remotemysql.com:3306/RE7esJnNWs','clientInfo_db':'mysql://9EDsNxvuTb:Wz0VOBdaZx@remotemysql.com:3306/9EDsNxvuTb'}
+app.config['SQLALCHEMY_BINDS'] = {'token_db': 'mysql://RE7esJnNWs:xsODaDhhpG@remotemysql.com:3306/RE7esJnNWs',
+                                  'clientInfo_db': 'mysql://9EDsNxvuTb:Wz0VOBdaZx@remotemysql.com:3306/9EDsNxvuTb'}
 
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-	__tablename__ = "password2"
-	mail = db.Column('email', db.String, primary_key=True,default='tst@aa.aa')
-	password = db.Column('login', db.String)
 
+class User(db.Model):
+    __tablename__ = "password2"
+    mail = db.Column('email', db.String, primary_key=True,
+                     default='tst2@aa.aa')
+    password = db.Column('login', db.String)
 
 
 class ConnectClient(db.Model):
-	__bind_key__='token_db'
-	__tablename__ = 'token'
+    __bind_key__ = 'token_db'
+    __tablename__ = 'token'
 
-	Email = db.Column('email', db.String, primary_key=True,default='tst@aa.aa')
-	Token = db.Column('token', db.Integer)
+    Email = db.Column('email', db.String, primary_key=True,
+                      default='tst2@aa.aa')
+    Token = db.Column('token', db.Integer)
 
 
 class Client(db.Model):
 
-	__bind_key__='clientInfo_db'
-	__tablename__ = 'utilisateur2'
+    __bind_key__ = 'clientInfo_db'
+    __tablename__ = 'utilisateur2'
 
-	Email = db.Column('email', db.String, primary_key=True,default='tst@aa.aa')
-	Name = db.Column('nom', db.String)
-	Sex=db.Column('sex', db.String)
-	LastName = db.Column('prénom', db.String)
-	CurrentBalance = db.Column('balance', db.String)
-	Incomes = db.Column('incomes', db.String)
-	Expenses = db.Column('expenses', db.String)
+    Email = db.Column('email', db.String, primary_key=True,
+                      default='tst2@aa.aa')
+    Name = db.Column('nom', db.String)
+    Sex = db.Column('sexe', db.String)
+    LastName = db.Column('prénom', db.String)
+    CurrentBalance = db.Column('balance', db.String)
+    Incomes = db.Column('incomes', db.String)
+    Expenses = db.Column('expenses', db.String)
